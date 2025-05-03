6b25a3269e4bfae7fc59aed775bc719861af85f5
camlogical <57839516+camlogical@users.noreply.github.com>
2025-05-03 17:39:42 +0700

    Update telegram_land_checker.py

--- telegram_land_checker.py
+++ telegram_land_checker.py
@@ -176,7 +176,7 @@ def scrape_land_data(land_number: str) -> dict:
     post_url = "https://miniapp.mlmupc.gov.kh/search"
 
     headers_common = {
-        "User-Agent": "Mozilla/5.0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate, br",
